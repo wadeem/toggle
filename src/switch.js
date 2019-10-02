5435a9946049568638102345903f4d0335009eaf
max3d <vadim@unikivi.ee>
2019-10-02 23:02:54 +0300

    standard version of setState

--- src/switch.js
+++ src/switch.js
@@ -2,25 +2,25 @@ import React from 'react';
 import './css/switch.css';
 
 export default class Switch extends React.Component {
+
     state = {
         toggle: false
     };
 
     toggle = () => {
-        this.setState({
-            toggle: !this.state.toggle
-        });
+        this.setState(state => ({
+            toggle: !state.toggle
+        }))
     };
 
     render() {
-        const buttonClassName = this.state.toggle
+        const btnClassName = this.state.toggle
             ? 'toggle-btn'.concat(' toggle-btn-on')
-            : 'toggle-btn'.concat(' toggle-btn.off');
+            : 'toggle-btn'.concat(' toggle-btn-off');
+
         return <div>
             <input className='toggle-input'/>
-            <button className={buttonClassName}
-                    onClick={this.toggle}
-            />
+            <button className={btnClassName} onClick={this.toggle}/>
         </div>
     }
 }
